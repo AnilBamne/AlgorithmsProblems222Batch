c8411624b2e4813b5efe4057c1952336c522032d
AnilBamne <anilbamne20@gmail.com>
2022-12-19 17:09:37 +0530

    Adding:BinarySearchWordFromWordList code

---
+++ AlgorithmAndDSPrograms/DemoPrograms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDSPrograms
+{
+    public class DemoPrograms
+    {
+        /// <summary>
+        /// Globally defining array of dtrings to store words from the file
+        /// </summary>
+        public string[] wordsArray;
+
+        /// <summary>
+        /// Fetching data from file and searching for word
+        /// </summary>
+        public static void BinarySearch()
+        {
+            DemoPrograms demo=new DemoPrograms();
+            string path = @"C:\Users\HP\Desktop\RFP222\AlgorithmAndDSPrograms\AlgorithmAndDSPrograms\DataFile.txt";
+            Console.WriteLine("Enter the word for searching in file");
+            string word = Console.ReadLine();
+            string[] lines=File.ReadAllLines(path);
+            
+            foreach(string line in lines)
+            {
+                demo.wordsArray = line.Split(",");
+            }
+            //Comparing 
+            bool isFound=false;
+            foreach(string item in demo.wordsArray)
+            {
+                if (item.Equals(word))
+                {
+                    isFound=true;
+                    break;
+                }
+                
+            }
+            if (isFound)
+            {
+                Console.WriteLine("{0} found", word);
+            }
+            else
+            {
+                Console.WriteLine("{0} Not found", word);
+            }
+        }
+    }
+}
--- AlgorithmAndDSPrograms/Program.cs
+++ AlgorithmAndDSPrograms/Program.cs
@@ -8,6 +8,17 @@ namespace AlgorithmAndDSPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to data structure and algorithm practice programs");
+            Console.WriteLine("Choose option\n1 Binary search for word\n2 Bubble sort\n3 Merge sort");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    DemoPrograms.BinarySearch();
+                    break;
+                case 2:
+                    break;
+            }
+            
         }
     }
 }
