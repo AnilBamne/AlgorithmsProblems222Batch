f17d29184579ef6e6c773c945f5f311d4f3656f1
AnilBamne <anilbamne20@gmail.com>
2022-12-19 15:09:04 +0530

    Adding:Welcome Messange

---
+++ AlgorithmAndDSPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace AlgorithmAndDSPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure and algorithm practice programs");
+        }
+    }
+}
